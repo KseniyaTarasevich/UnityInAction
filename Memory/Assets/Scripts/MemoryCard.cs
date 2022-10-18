37359c327caa662abc1c3e1dba566a8d3f8f8812
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-10-18 21:52:02 +0300

    add all functional

--- Memory/Assets/Scripts/MemoryCard.cs
+++ Memory/Assets/Scripts/MemoryCard.cs
@@ -1,22 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MemoryCard : MonoBehaviour
 {
     [SerializeField] private GameObject cardBack;
-    //[SerializeField] private Sprite image;
+    [SerializeField] private SceneController controller;
 
-    private void Start()
+    private int _id;
+
+    public int id
     {
-      //  GetComponent<SpriteRenderer>().sprite = image;
+        get { return _id; }
     }
 
     private void OnMouseDown()
     {
-        if(cardBack.activeSelf)
+        if (cardBack.activeSelf && controller.canReveal)
         {
             cardBack.SetActive(false);
+            controller.CardRevealed(this);
         }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
     }
 }
