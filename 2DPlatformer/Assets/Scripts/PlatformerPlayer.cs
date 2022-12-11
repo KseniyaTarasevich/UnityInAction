c19f62fc7c5af508db0257ba064ffe9b212162fb
KseniyaTarasevich <tarasevichkseniya11@gmail.com>
2022-12-11 20:38:23 +0300

    add script for camera following add moving platform

--- 2DPlatformer/Assets/Scripts/PlatformerPlayer.cs
+++ 2DPlatformer/Assets/Scripts/PlatformerPlayer.cs
@@ -38,12 +38,41 @@ public class PlatformerPlayer : MonoBehaviour
             grounded = true;
         }
 
+        _rb.gravityScale = grounded && deltaX == 0 ? 0 : 1;
+
         if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
+        MovingPlatform platform = null;
+
+        if (hit != null)
+        {
+            platform = hit.GetComponent<MovingPlatform>();
+        }
+
+        if (platform != null)
+        {
+            transform.parent = platform.transform;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+
         _animator.SetFloat("speed", Mathf.Abs(deltaX));
+
+        Vector3 pScale = Vector3.one;
+
+        if (platform != null)
+        {
+            pScale = platform.transform.localScale;
+        }
+        if (deltaX != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX) / pScale.x, 1 / pScale.y, 1);
+        }
 
         if (!Mathf.Approximately(deltaX, 0))
         {
