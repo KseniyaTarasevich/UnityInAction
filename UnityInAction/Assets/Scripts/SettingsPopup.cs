467511538fcb213f1fa229397d59b6d9f2a8f5cd
KseniyaTarasevich <tarasevichkseniya11@gmail.com>
2023-01-07 16:10:12 +0300

    add playerprefs for slider speed

--- UnityInAction/Assets/Scripts/SettingsPopup.cs
+++ UnityInAction/Assets/Scripts/SettingsPopup.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsPopup : MonoBehaviour
 {
+    [SerializeField] private Slider speedSlider;
+
+    private void Start()
+    {
+        speedSlider.value = PlayerPrefs.GetFloat("speed", 1);
+    }
     public void Open()
     {
         gameObject.SetActive(true);
@@ -12,5 +19,15 @@ public class SettingsPopup : MonoBehaviour
     public void Close()
     {
         gameObject.SetActive(false);
+    }
+
+    public void OnSubmitName(string name)
+    {
+        Debug.Log(name);
+    }
+
+    public void OnSpeedValue(float speed)
+    {
+        Debug.Log("Speed: " + speed);
     }
 }
